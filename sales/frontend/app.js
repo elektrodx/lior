0a4eb85c59a5d3d4574d8d409b09b1887ddda414
elektrodx <elektrodx@gmail.com>
2015-08-09 18:56:32 -0400

    instalacion de dependencias para trabajo con React

---
+++ sales/frontend/app.js
@@ -0,0 +1,9 @@
+/*
+ * Modules Dependiencies
+ */
+
+import React from 'react';
+import FormMain from './components/FormMain';
+
+
+React.render(<FormMain/>, document.getElementById('bcontainer'));
---
+++ sales/frontend/components/FormMain.js
@@ -0,0 +1,13 @@
+/*
+ * Modules Dependiencies
+ */
+
+import React from 'react';
+
+
+export default class FormMain extends React.Component {
+	render(){
+		return <div className="form"> Hola React
+		</div>
+	}
+}
