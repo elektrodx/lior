3adad7eb8712b75a8f3269f2579da050b1cffe12
Manuel Mercado <manu_el_3@hotmail.com>
2016-07-19 18:47:49 -0400

    arreglado el home (server)
    ademas se ha agregado el parts_left a la forma del stock

--- stock/forms.py
+++ stock/forms.py
@@ -13,7 +13,7 @@ class StockForm(ModelForm):
     class Meta:
         model = Stock
         autocomplete_fields = ("description",)
-        fields = ('brand', 'qty', 'description', 'code', 'date_end', 'price_base', 'units', 'place', 'note', 'parts')
+        fields = ('brand', 'qty', 'description', 'code', 'date_end', 'price_base', 'units', 'place', 'note', 'parts', 'parts_left')
         widgets = {
            #"description":autocomplete_light.TextWidget("StockDescAutocomplete"),
         }
