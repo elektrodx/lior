953404aedcda24d3f12f3a1a0bf49b31b9564dad
elektrodx <elektrodx@gmail.com>
2016-07-21 22:26:20 -0400

    Graba todos los datos add_shop
    se graban los baton en shop, shopdetail, actualización de precios y
    cantidad en stock y el egreso en expenses

--- sales/views.py
+++ sales/views.py
@@ -7,7 +7,7 @@ from relations.models import Customers
 from stock.models import Stock
 from assets.models import Income
 from django.contrib.auth.decorators import login_required
-from wkhtmltopdf.views import PDFTemplateView, PDFTemplateResponse
+#from wkhtmltopdf.views import PDFTemplateView, PDFTemplateResponse
 import os, json
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
@@ -25,22 +25,22 @@ def add_sale(request):
 		form = SalesForm()
 	return render(request, 'add_sale.html', {'form': form })
 
-class Invoice(PDFTemplateView):
-    template='invoice.html'
-    q_sales = Sales.objects.all().last()
-    d_sales = SalesDetail.objects.filter(sale=q_sales.pk)
-    context = { 'pk': q_sales.pk, 'customer': q_sales.customer, 'date': q_sales.date, 'total': q_sales.amount, 'items': d_sales }
-    def get(self, request):
-        response = PDFTemplateResponse(request=request,
-                                       template=self.template,
-                                       filename="hello.pdf",
-                                       context= self.context,
-                                       show_content_in_browser=True,
-                                       cmd_options={'margin-top': 5,
-                                       'orientation': 'Landscape',
-                                       'page-size': 'Letter'},
-                                       )
-        return response
+#class Invoice(PDFTemplateView):
+#    template='invoice.html'
+#    q_sales = Sales.objects.all().last()
+#    d_sales = SalesDetail.objects.filter(sale=q_sales.pk)
+#    context = { 'pk': q_sales.pk, 'customer': q_sales.customer, 'date': q_sales.date, 'total': q_sales.amount, 'items': d_sales }
+#    def get(self, request):
+#        response = PDFTemplateResponse(request=request,
+#                                       template=self.template,
+#                                       filename="hello.pdf",
+#                                       context= self.context,
+#                                       show_content_in_browser=True,
+#                                       cmd_options={'margin-top': 5,
+#                                       'orientation': 'Landscape',
+#                                       'page-size': 'Letter'},
+#                                       )
+#        return response
 
 @csrf_exempt
 def sales_postjson(request):
--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from .forms import *
 from .models import *
 from relations.models import Providers
+from assets.models import Expenses
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
@@ -33,9 +34,22 @@ def shop_add(request):
 		shopn.price_extra=price_extra
 		shopn.total_price=total_price
 		shopn.payed = total_price + price_extra
-		pdb.set_trace()
 		shopn.save()
+		expensesN = Expenses(provider_id=provider, amount=total_price+price_extra, concept='Compra de Productos')
+		expensesN.save()
 		items = request.POST.dict()
+		data = ast.literal_eval(items['items'])
+		lastshop = Shop.objects.last()
+		for index in data:
+			item_pk = Stock.objects.get(code=index[0])
+			item_pk.qty = item_pk.qty + int(index[2])
+			if item_pk.price_base < float(index[5]):
+				item_pk.price_base = float(index[5])
+			item_pk.save()	
+			item_qty = int(index[2])
+			item_price_shop = float(index[5])
+			shop_detailN = ShopDetail(shop_id=lastshop.pk, item_id=item_pk.pk, qty=item_qty, place_id=1, price_shop=item_price_shop)
+			shop_detailN.save()
 		return HttpResponse(status=201)
 	else:			
 		return HttpResponse(status=501)
